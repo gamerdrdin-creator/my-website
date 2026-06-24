9eb22faa1e54e0e6b316a4868a2027af0d8f309a
gamerdrdin-creator <gamerdrdin@gmail.com>
2026-06-24 16:11:44 +0530

    Update vite.config.js

--- foundrai/frontend/vite.config.js
+++ foundrai/frontend/vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  base:"/my-website/",
+  server: { 
+    port: 5173 },
 });
